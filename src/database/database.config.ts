f27a61beb8708c53cdbe4c75adadb7ba65f71720
AndreyBS1 <andrey.babenkov.00@gmail.com>
2023-12-10 07:00:01 +0300

    feat: add database config

---
+++ src/database/database.config.ts
@@ -0,0 +1,23 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm'
+import { join } from 'path'
+import { DataSource, DataSourceOptions } from 'typeorm'
+
+const dataSourceConfig: DataSourceOptions = {
+  type: process.env.DATABASE_TYPE as any,
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT),
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  entities: ['dist/**/*.entity.js'],
+  migrations: [join(__dirname, 'migrations', '*.{ts,js}')],
+  migrationsTableName: 'typeorm_migrations',
+  migrationsTransactionMode: 'all',
+}
+
+export const databaseConfig: TypeOrmModuleOptions = {
+  ...dataSourceConfig,
+  autoLoadEntities: true,
+}
+
+export const dataSource = new DataSource(dataSourceConfig)
