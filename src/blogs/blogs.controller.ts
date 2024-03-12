906e0e623205a13b48a04447d346561b47969581
PS-Maxim <maxim.yarullin@gmail.com>
2024-03-12 20:46:03 +0300

    feat: blogs and image api

---
+++ src/blogs/blogs.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common'
+import { BlogsService } from './blogs.service'
+import { BlogDto } from './dto/blog.dto'
+import { PublicRoute } from 'src/infrastructure/decorators/public-route.decorator'
+
+@Controller('blogs')
+export class BlogsController {
+  constructor(private readonly blogsService: BlogsService) {}
+
+  @PublicRoute()
+  @Post()
+  create(@Body() blogDto: BlogDto) {
+    return this.blogsService.create(blogDto)
+  }
+
+  @PublicRoute()
+  @Get()
+  findAll() {
+    return this.blogsService.findAll()
+  }
+
+  @PublicRoute()
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.blogsService.findOne(+id)
+  }
+
+  @PublicRoute()
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() blogDto: BlogDto) {
+    return this.blogsService.update(+id, blogDto)
+  }
+
+  // @RequiredRoles(UserRolesEnum.SuperAdmin)
+  @PublicRoute()
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.blogsService.remove(+id)
+  }
+}
---
+++ src/blogs/blogs.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { BlogDto } from './dto/blog.dto'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Blog } from './entities/blog.entity'
+import { Repository } from 'typeorm'
+
+@Injectable()
+export class BlogsService {
+  constructor(
+    @InjectRepository(Blog)
+    private blogRepository: Repository<Blog>,
+  ) {}
+
+  create(blogDto: BlogDto): Promise<Blog> {
+    const blog = this.blogRepository.create(blogDto)
+    return this.blogRepository.save(blog)
+  }
+
+  findAll(): Promise<Blog[]> {
+    return this.blogRepository.find({ order: { createdAt: 'ASC' } })
+  }
+
+  async findOne(id: number): Promise<Blog> {
+    const blog = await this.blogRepository.findOneBy({ id })
+    if (!blog) {
+      throw new NotFoundException(`Blog with id ${id} not found`)
+    }
+    return blog
+  }
+
+  async update(id: number, blogDto: BlogDto): Promise<Blog> {
+    const blog = await this.blogRepository.findOneBy({ id })
+    if (!blog) {
+      throw new NotFoundException(`Blog with id ${id} not found`)
+    }
+    const updatedBlog = { ...blog, ...blogDto }
+    return this.blogRepository.save(updatedBlog)
+  }
+
+  async remove(id: number): Promise<void> {
+    const isBlogExisting = await this.blogRepository.exist({
+      where: { id },
+    })
+    if (!isBlogExisting) {
+      throw new NotFoundException(`Blog with id ${id} not found`)
+    }
+    await this.blogRepository.delete(id)
+  }
+}
